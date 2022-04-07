1ce23539c73cf4d06b1963ac18098e3a749b97ad
Jimena Narváez <101615158+jimena202@users.noreply.github.com>
2022-04-06 23:02:39 -0500

    este es mi primer commit

---
+++ src/test/java/ista/edu/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package ista.edu.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
